03fbd814b65a20455a981643fa09a4a059d83801
Fouad <fouadnasser1998@gmail.com>
2022-08-13 09:04:02 +0200

    Empty-Commit

---
+++ ps.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin >>x>>y;
+
+    printf("%d + %d = %d\n",x,y,x+y);
+    printf("%d * %d = %d\n",x,y,x*y);
+    printf("%d - %d = %d\n",x,y,x-y);
+
+    return 0;
+}
